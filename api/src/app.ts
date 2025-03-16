1b4ee0f0f673c39b0f72bea9087278f8daf5fa39
Baptiste Georget <baptistegeorget@icloud.com>
2025-03-17 00:06:32 +0100

    Add docker config for web service

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,5 @@
 import { app } from "./lib/express.js";
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Application start on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Application start on port ${process.env.PORT || 8000}`);
 });
