fe42d782d18d4983651c566a0b8bf1f5514dd370
Baptiste Georget <baptistegeorget@icloud.com>
2025-03-26 21:46:35 +0100

    Update libraries

--- api/src/lib/pg.ts
+++ api/src/lib/pg.ts
@@ -1,16 +1,12 @@
 import pg from "pg";
 
 export const pool = new pg.Pool({
-  user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT ? parseInt(process.env.POSTGRES_PORT) : undefined,
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
 });
-
-pg.types.setTypeParser(1082, val => new Date(val));
-pg.types.setTypeParser(1114, val => new Date(val));
-pg.types.setTypeParser(1184, val => new Date(val));
