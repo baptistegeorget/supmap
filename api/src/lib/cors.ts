a5c4f3b3fb6ff579f82d9119f29d27a898b9037a
Baptiste Georget <baptistegeorget@icloud.com>
2025-04-01 14:02:46 +0200

    Fix cors errors

--- api/src/lib/cors.ts
+++ api/src/lib/cors.ts
@@ -1,7 +1,11 @@
 import { CorsOptions } from "cors";
 
 export const options: CorsOptions = {
-  origin: "http://localhost",
+  origin: [
+    "http://localhost",
+    "http://web",
+    "http://web:3000",
+  ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
