0194cc0b4c6dc0537034f126ae0d40de1e49988c
Baptiste Georget <baptistegeorget@icloud.com>
2025-04-22 16:27:47 +0200

    test

--- api/src/routes/v1/auth/index.ts
+++ api/src/routes/v1/auth/index.ts
@@ -85,10 +85,12 @@ router.post("/auth/signin", async (req, res) => {
 
 router.get("/auth/google", async (req, res) => {
   try {
+    const redirectUri = req.headers["User-Agent"]?.includes("Mobile") || req.headers["User-Agent"]?.includes("Tablet") || req.headers["User-Agent"]?.includes("Android") ? GOOGLE_REDIRECT_URI_MOBILE : GOOGLE_REDIRECT_URI_WEB;
+    console.log("Google redirect URI : ", redirectUri);
     const oAuth2Client = new OAuth2Client(
       GOOGLE_CLIENT_ID,
       GOOGLE_CLIENT_SECRET,
-      req.headers["User-Agent"]?.includes("Mobile") || req.headers["User-Agent"]?.includes("Tablet") || req.headers["User-Agent"]?.includes("Android") ? GOOGLE_REDIRECT_URI_MOBILE : GOOGLE_REDIRECT_URI_WEB
+      redirectUri
     );
 
     const url = oAuth2Client.generateAuthUrl(
